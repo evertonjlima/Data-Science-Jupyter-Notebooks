36988cc1ce218cfb51a50aa28333e0b9df083d60
evertonjlima <everton.job.lima@gmail.com>
2016-02-02 15:57:05 +0100

    added training set images

---
+++ Digit-Recognizer/plotDigit.R
@@ -0,0 +1,42 @@
+plotDigit <- function( r ){
+# Plots an entry of the MNIST dataset.
+#
+# Args:
+#  p   data.frame   Labeled entry of the mnist dataset (1 by 785 vector).
+#
+# Return:
+#   None    function is executed for side effects only.
+#
+  # Helper functions  
+  rotate <- function(x) t(apply(x, 1, rev))
+  
+  # Convert entry into a 28x28 matrix
+
+  label <- ""
+  
+  if(!is.null(dim(r))){
+    digit <- matrix(as.numeric(r[,-1]),28,28)
+    label <- r[,1]
+  }
+  else{
+    digit <- matrix(r[-1],28,28)
+    label <- r[1]
+  }
+
+  # Plot
+  image(rotate(digit), col = palette(gray(seq(0, .2, len=32))),
+        zlim = c(1,255) , axes = F, asp = 1, main = label) 
+}
+
+generatePlots <- function(m, range=15,s=T){
+
+  for( e in 1:range){
+    plotDigit(m[e,])
+    
+    if(s){
+      dev.copy(png,paste("train",e,".png",sep = ""))
+      dev.off()
+    }
+  }
+  
+}
--- Digit-Recognizer/readme.md
+++ Digit-Recognizer/readme.md
@@ -1,6 +1,10 @@
 # Digit Recognizer: MNIST Dataset
 
-[About](https://www.kaggle.com/c/digit-recognizer)
+This repo is a collection of scripts and analysis related to [Kaggle's Digit Recognizer](https://www.kaggle.com/c/digit-recognizer) challenge.
 
+### Exploring the data.
+
+
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png )
 
 
