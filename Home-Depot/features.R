736c1527f3f87c9a2e0fa7d1071b5628e5ebad70
evertonjlima <everton.job.lima@gmail.com>
2016-04-29 16:59:39 +0200

    starting home depot project

---
+++ Home-Depot/features.R
@@ -0,0 +1,33 @@
+# Features
+# @author: Everton J Lima
+# @data: April 29, 2016
+# @revised: NA
+#
+# jaccard
+#
+#
+#
+
+jaccard <- function(x,y){
+  # jaccard
+  # Returns the jaccard coefficient as calculated by the size of the intersection, divided by the size
+  # of the union of two texts.
+  #
+  # args:
+  #  x  character  text used for measuring.
+  #  y  character  as above, text used for measuring.
+  #
+  # return
+  #  numeric  jaccard distance, value within [0,1].
+  # 
+  # example -  jaccard("Spam Spam Spam Spam","Bacon Spam")
+  #
+  
+  str1 = unlist(strsplit(tolower(x),split = " "))
+  str2 = unlist(strsplit(tolower(y),split = " "))
+  
+  union_size = length(unlist(unique(c(str1,str2))))
+  intersection_size = length(unlist(intersect(str1,str2)))
+  
+  intersection_size/union_size
+}
---
+++ Home-Depot/readme.md
@@ -0,0 +1,43 @@
+# Home Depot Kaggle Challenge
+
+# Planning
+
+## W18
+[] - Overview of Data
+[x] - Feature: Jaccard Distance.
+[x] - Simple Linear Regression Using 1 Feature.
+[] - Preprocessing Data
+[] - Feature: Tf-Itf
+[] - Feature: 
+[] - Course Lectures.
+
+## W19
+[] - 
+
+## W20
+[] - 
+
+## W21
+[] - 
+
+## W22
+[] - 
+
+## W23
+[] - 
+
+## W24
+[] - 
+
+## W25
+[] - 
+
+## W26
+[] - 
+
+## W27
+[] - 
+
+## W28
+[] - 
+
